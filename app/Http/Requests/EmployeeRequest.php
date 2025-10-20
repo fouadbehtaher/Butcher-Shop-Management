4a38db72aea3995b4191135f3858530f73eb6737
Fouad Beh Taher <130560541+fouadbehtaher@users.noreply.github.com>
2025-10-20 06:48:46 +0300

    Update EmployeeRequest.php

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -1,1 +1,36 @@
+<?php
+// app/Http/Requests/EmployeeRequest.php
 
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeeRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'position' => 'required|string|max:50',
+            'salary' => 'required|numeric|min:0',
+            'phone' => 'required|string|max:20',
+            'hire_date' => 'required|date',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'حقل الاسم مطلوب',
+            'position.required' => 'حقل الوظيفة مطلوب',
+            'salary.required' => 'حقل الراتب مطلوب',
+            'phone.required' => 'حقل الهاتف مطلوب',
+            'hire_date.required' => 'حقل تاريخ التعيين مطلوب',
+        ];
+    }
+}
