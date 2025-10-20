bb6a3c77850b81036dd802b7b0e5342985485c45
Fouad Beh Taher <130560541+fouadbehtaher@users.noreply.github.com>
2025-10-20 06:48:10 +0300

    Add Employee model with fillable attributes and methods

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -1,1 +1,36 @@
+<?php
+// app/Models/Employee.php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'position',
+        'salary',
+        'phone',
+        'hire_date',
+        'status'
+    ];
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
+    public function getWeeklyAttendance($weekStart)
+    {
+        return $this->attendances()
+            ->whereBetween('date', [
+                $weekStart,
+                \Carbon\Carbon::parse($weekStart)->addDays(6)
+            ])
+            ->get();
+    }
+}
